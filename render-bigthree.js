c5fe73b2f5d4b5260f13a9d96f512a89183eddc5
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-15 15:45:05 -0700

    fixed array4

--- render-bigthree.js
+++ render-bigthree.js
@@ -6,21 +6,24 @@ export function renderBigThree(zodiacData) {
 	const attributesListEl = document.createElement('ul')
 	const imgEl = document.createElement('img');
 
-	bigThreeEl.classList.add('zodiac');
+	bigThreeEl.classList.add('.bigThree');
 
-	signEl.textContent = `Sign: ${zodiacData.sign}`;
-	planetEl.textContent = `Planet: ${zodiacData.planet}`;
-	placementEl.textContent = `Placement: ${zodiacData.planet}`;
-	imgEl.src = `assets/${zodiacData.name}.jpg`;
+	signEl.textContent = `Sign: ${zodiacData.Sign}`;
+	planetEl.textContent = `Planet: ${zodiacData.Planet}`;
+	placementEl.textContent = `Placement: ${zodiacData.Placement}`;
+	attributesListEl.textContent = `Attributes:`;
+	imgEl.src = `assets/${zodiacData.Sign}.jpg`;
 
-	for (let i of zodiacData.attributes) {
+	for (let i of zodiacData.Attributes) {
 
 		const attributeEl = document.createElement('li');
 		attributeEl.textContent = i;
+		console.log(i);
 
 		attributesListEl.append(attributeEl);
+	}
 
-		bigThreeEl.append(signEl, planetEl, placementEl, imgEl, attributesListEl)
+	bigThreeEl.append(signEl, planetEl, placementEl, imgEl, attributesListEl)
 
-		return bigThreeEl;
-	} 
+	return bigThreeEl;
+}
--- zodiac-data.js
+++ zodiac-data.js
@@ -1,20 +1,21 @@
-export const bigThree [
+
+export const bigThree = [
 	{
 		Sign: 'Scorpio',
-		Planet: 'Pluto'
+		Planet: 'Pluto',
 		Placement: 'Sun',
 		Attributes: ['loyal', 'sharp', 'intense', 'vibrant']
-},
-{
-	Sign: 'Libra',
+	},
+	{
+		Sign: 'Libra',
 		Planet: 'Venus',
-			Placement: 'Rising',
-				Attributes: ['engaging', 'choosy', 'glamourous', 'chatty']
-},
-{
-	Sign: 'Aquarius',
+		Placement: 'Rising',
+		Attributes: ['engaging', 'choosy', 'glamourous', 'chatty']
+	},
+	{
+		Sign: 'Aquarius',
 		Planet: 'Uranus',
-			Placement: 'Moon',
-				Attributes: ['welcoming', 'tolerant', 'caring', 'unconventional']
-}
+		Placement: 'Moon',
+		Attributes: ['welcoming', 'tolerant', 'caring', 'unconventional']
+	}
 ]
